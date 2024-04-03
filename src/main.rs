1502d320ac8919a79488c875f240b68713b4b610
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-04-04 04:20:46 +0900

    program v1

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+use bsp::{
+    entry,
+    hal::{
+        // dma::Channel,
+        pwm::{FreeRunning, Slices},
+    },
+};
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+use embedded_hal::PwmPin;
 use panic_probe as _;
 
 // Provide an alias for our BSP so we can switch targets quickly.
@@ -62,15 +69,84 @@ fn main() -> ! {
     // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
     // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
     // in series with the LED.
-    let mut led_pin = pins.led.into_push_pull_output();
+
+    let sw_test = pins.gpio0.into_pull_down_input();
+    let door_sw = pins.gpio15.into_pull_down_input();
+    let mut power_led = pins.led.into_push_pull_output();
+    let mut led = pins.gpio10.into_push_pull_output();
+    let pwm_pin = pins.gpio16.into_push_pull_output();
+    let pwm_slices = Slices::new(pac.PWM, &mut pac.RESETS);
+    let mut pwm = pwm_slices.pwm0;
+    pwm.set_ph_correct();
+    pwm.enable();
+    let channel_a = &mut pwm.channel_a;
+    let _channel_pin_a = channel_a.output_to(pwm_pin);
+    channel_a.set_duty(1420 / 6);
+    power_led.set_high().ok().unwrap();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        delay.delay_ms(40);
+        if door_sw.is_low().ok().unwrap() || sw_test.is_high().ok().unwrap() {
+            led.set_high().ok().unwrap();
+            pwm.set_top(868 - 1);
+            pwm.set_div_int(200);
+            pwm.set_div_frac(0);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(1064 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(1420 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(1064 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(948 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(710 - 2);
+
+            delay.delay_ms(600);
+
+            pwm.set_top(0);
+            delay.delay_ms(200);
+
+            pwm.set_top(1899 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(948 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(845 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(948 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(1420 - 1);
+
+            delay.delay_ms(400);
+
+            pwm.set_top(1064 - 1);
+
+            delay.delay_ms(800);
+
+            pwm.set_top(0);
+            led.set_low().ok().unwrap();
+
+            delay.delay_ms(1800)
+        } else {
+        }
     }
 }
 
